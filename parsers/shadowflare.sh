2cf9129905bc544e812196eb15b5c27521b3cb32
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-03-27 18:18:21 +0100

    Adding ShadowFlare and Stormcoast-Fortress parsers

---
+++ parsers/shadowflare.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+currdir="$(dirname "$0")"
+source "$currdir/_common.sh"
+
+url="$1"
+filter="${2:-.*}"
+maxelems="${3:-15}"
+maxtextlen="${4:-4096}"
+
+curl -sL "$url" | tr '\r' '\n' | awk '
+BEGIN {
+    Debug = 0
+
+    Base_url = "https://sfsrealm.hopto.org/"
+    split("January February March April May June July August September October November December", month)
+    for (i in month) {
+        month_nums[month[i]] = i
+    }
+
+    Title     = ""
+    Date      = ""
+    Published = ""
+    Author    = ""
+    Url       = ""
+    Items     = ""
+
+    Is_reading_article = 0
+}
+{
+    if ($0 ~ "^<center><font size=4>Current News</font><p></center>")
+        Is_reading_article = 1
+    if (!Is_reading_article)
+        next
+
+    match($0, /^<font face=.arial. color=.#50FFFF. size=.2.><b>(.+)<\/b><\/font><P>/, a)
+    if (RSTART != 0) {
+        Date = a[1]
+        split(Date, a)
+        sub(/,/, "", a[3])
+        m = int(month_nums[a[2]])
+        if (m < 10)
+            m = "0" m
+        Date = a[4] "-" m "-" a[3]
+        if (Debug) print "Date: " Date > "/dev/stderr"
+    }
+
+    match($0, /^<td bgcolor=808080><B><A NAME="(.+)"><FONT COLOR="#FFEEDD" SIZE="2" FACE="arial">(.+)<\/FONT><\/B>/, a)
+    if (RSTART != 0) {
+        Url   = a[1]
+        Title = a[2]
+        if (Debug) print "Url: " Url ", Title: " Title > "/dev/stderr"
+    }
+
+    match($0, /^<FONT COLOR="#FFEEDD" SIZE="2" FACE="arial"> - <A HREF="[^">]*">(.+)<\/A> - ([0-9:]+)<\/FONT><\/td>/, a)
+    if (RSTART != 0) {
+        $0 = ""
+        Author = a[1]
+        Published = Date "T" a[2] ":00Z"
+        if (Debug) print "Author: " Author ", Published: " Published > "/dev/stderr"
+    }
+
+    match($0, /^<FONT COLOR="#FFEEDD" SIZE="2" FACE="arial">(.+)/, a)
+    if (RSTART != 0) {
+        text = trim(a[1])
+        gsub(/\\/, "\\\\", text)
+        gsub(/"/,  "\\\"", text)
+        gsub(/\t/, "    ", text)
+
+        i = "  {\n"
+        i = i "    \"id\": \"" Url "\",\n"
+        i = i "    \"title\": \"" Title "\",\n"
+        i = i "    \"user\": \"" Author "\",\n"
+        i = i "    \"url\": \"" Base_url "#" Url "\",\n"
+        i = i "    \"created\": \"" Published "\",\n"
+        i = i "    \"text\": \"" text "\"\n"
+        i = i "  }"
+
+        Items     = Items (Items == "" ? "" : ",\n") i
+        Title     = ""
+        Author    = ""
+        Url       = ""
+        Published = ""
+    }
+}
+END {
+    print "[\n" Items "\n]"
+}
+
+# Trims away any whitespace (i.e. space, tab, newlines, carrige-returns) from the left and right of given [string]
+function trim(string) {
+    sub(/^[ \t\r\n]+/,  "", string)
+    sub( /[ \t\r\n]+$/, "", string)
+    return string
+}' | filter_and_shrink "$filter" "$maxelems" "$maxtextlen"
---
+++ parsers/stormcoast-fortress.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+currdir="$(dirname "$0")"
+source "$currdir/_common.sh"
+
+url="$1"
+filter="${2:-.*}"
+maxelems="${3:-15}"
+maxtextlen="${4:-4096}"
+
+curl -sL "$url" | tr '\r' '\n' | tr "'", "\"" | awk '
+BEGIN {
+    Debug = 0
+
+    Base_url  = "http://www.stormcoast-fortress.net/"
+
+    Title     = ""
+    Published = ""
+    Author    = ""
+    Url       = ""
+    Item      = ""
+    Items     = ""
+
+    Is_reading_article = 0
+    Is_reading_item    = 0
+}
+{
+    if ($0 ~ "<td class=.main.>")
+        Is_reading_article = 1
+    if ($0 ~ "<a href=./news/Archive/.>News Archive</a>")
+        Is_reading_article = 0
+    if (!Is_reading_article)
+        next
+
+    match($0, /<a name=.(.*).><\/a><table class=.fancytable. cellspacing=.0.>/, a)
+    if (RSTART != 0) {
+        Url = a[1]
+        if (Debug) print "Url: " Url > "/dev/stderr"
+    }
+
+    match($0, /<td class=.fancycaption.>(.*)<\/td>/, a)
+    if (RSTART != 0) {
+        Title = a[1]
+        if (Debug) print "Title: " Title > "/dev/stderr"
+    }
+
+    match($0, /<td><div class=.fancytop.>&nbsp;Posted: (.*)<\/div><\/td>/, a)
+    if (RSTART != 0) {
+        Published = a[1]
+        split(Published, b)
+        split(b[1], c, "-")
+        if (length(c[1]) == 1)
+            c[1] = "0" c[1]
+        if (length(c[2]) == 1)
+            c[2] = "0" c[2]
+
+        Published = c[3] "-" c[2] "-" c[1] "T" b[2] "Z"
+        if (Debug) print "Published: " Published > "/dev/stderr"
+    }
+
+    match($0, /<td align=.right.width=.150.><div class=.fancybox darkbglink.>&nbsp;<a href=[^>]*>(.*)<\/a><\/div><\/td>/, a)
+    if (RSTART != 0) {
+        Author = a[1]
+        if (Debug) print "Author: " Author > "/dev/stderr"
+    }
+
+    match($0, /^<div class=.fancycontent.>(.*)/, a)
+    if (RSTART != 0) {
+        Is_reading_item = 1
+        $0   = ""
+        Item = a[1]
+    }
+
+    match($0, /(.*)<BR><br><\/div>/, a)
+    if (RSTART != 0) {
+        Is_reading_item = 0
+        Item = trim(Item "\n" a[1])
+
+        gsub(/\\/, "\\\\", Item)
+        gsub(/"/,  "\\\"", Item)
+        gsub(/\n/, "\\n", Item)
+        Item = remove_non_ascii(Item)
+
+        i = "  {\n"
+        i = i "    \"id\": \"" Url "\",\n"
+        i = i "    \"title\": \"" Title "\",\n"
+        i = i "    \"user\": \"" Author "\",\n"
+        i = i "    \"url\": \"" Base_url "#" Url "\",\n"
+        i = i "    \"created\": \"" Published "\",\n"
+        i = i "    \"text\": \"" Item "\"\n"
+        i = i "  }"
+
+        Items     = Items (Items == "" ? "" : ",\n") i
+        Item      = ""
+        Title     = ""
+        Author    = ""
+        Url       = ""
+        Published = ""
+    }
+
+    if (Is_reading_item) {
+        Item = Item "\n" $0
+    }
+}
+END {
+    print "[\n" Items "\n]"
+}
+
+function remove_non_ascii(text) {
+    gsub(/[^a-zA-Z0-9 "\.,!#\*<>()%_=:\-\\\/]/, "", text)
+    return text
+}
+
+# Trims away any whitespace (i.e. space, tab, newlines, carrige-returns) from the left and right of given [string]
+function trim(string) {
+    sub(/^[ \t\r\n]+/,  "", string)
+    sub( /[ \t\r\n]+$/, "", string)
+    return string
+}' | filter_and_shrink "$filter" "$maxelems" "$maxtextlen"
