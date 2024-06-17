421f8501038ad85c7c0cc8fe28f81d98c973d772
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-06-17 20:11:11 +0200

    Refactoring wardruna parser to use common awk functions

--- parsers/wardruna.sh
+++ parsers/wardruna.sh
@@ -7,19 +7,15 @@ filter="$2"
 maxelems="${3:-15}"
 maxtextlen="${4:-4096}"
 
-curl --connect-timeout 10 -sL "$url" | awk '
+curl --connect-timeout 10 -sL "$url" | tr '\r' '\n' | tr "'" "\"" | awk -i "parsers/_common.awk" '
 BEGIN {
     Debug = 0
-
-    split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec", month)
-    for (i in month) {
-        month_nums[month[i]] = i
-    }
 
     Title     = ""
     Published = ""
     Item      = ""
     Items     = ""
+    Author    = "Wardruna"
 
     Is_reading_article = 0
     Is_reading_item    = 0
@@ -40,15 +36,8 @@ BEGIN {
         gsub(/&ndash;/,  "—", Title)
         gsub(/&aacute;/, "á", Title)
 
-        split(pub, puba)
-        y = puba[3]
-        m = int(month_nums[substr(puba[2], 1, 3)])
-        if (m < 10)
-            m = "0" m
-        d = puba[1]
-        if (d < 10)
-            d = "0" d
-        Published = y "-" m "-" d "T00:00:00.000Z"
+        split(pub, a)
+        Published = make_iso_datetime(a[3], a[2], a[1], "00", "00", "00", "000")
 
         $0 = ""
         if (Debug) print "Read Title: '" Title "', Published: " Published > "/dev/stderr"
@@ -57,29 +46,16 @@ BEGIN {
     if ($0 ~ "<br><br><br><br>") {
         Is_reading_item = 0
 
-        i = "  {\n"
-        i = i "    \"id\": \"" Title "_" Published "\",\n"
-        i = i "    \"title\": \"" Title "\",\n"
-        i = i "    \"created\": \"" Published "\",\n"
-        i = i "    \"text\": \"" trim(Item) "\"\n"
-        i = i "  }"
+        Item = make_item(Item, Title "_" Published, Title, Author, null, Published)
 
-        Items = Items (Items == "" ? "" : ",\n") i
+        Items = Items (Items == "" ? "" : ",\n") Item
         Item  = ""
     }
     if ($0 != "" && Is_reading_item) {
-        gsub(/"/, "\\\"", $0) # Escape quotes
-        gsub(/	/, " ",   $0) # Fix weird space
-        Item = Item (Item == "" ? "" : "\\n") trim($0)
+        Item = Item (Item == "" ? "" : "\n") trim($0)
     }
 }
 END {
     print "[\n" Items "\n]"
 }
-
-# Trims away any whitespace (i.e. space, tab, newlines, carrige-returns) from the left and right of given [string]
-function trim(string) {
-    sub(/^[ \t\r\n]+/,  "", string)
-    sub( /[ \t\r\n]+$/, "", string)
-    return string
-}' | filter_and_shrink "$filter" "$maxelems" "$maxtextlen"
+' | filter_and_shrink "$filter" "$maxelems" "$maxtextlen"
